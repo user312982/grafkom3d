9118f94c2e1c56f72b984791085eaa883c8d9464
thegouhund <alexandresapu1@gmail.com>
2025-05-19 12:24:17 +0800

    refactor: separate physics function

--- src/object_3d.py
+++ src/object_3d.py
@@ -7,6 +7,7 @@ class Object3D:
         position=None,
         velocity=None,
         mass=1.0,
+        friction=0.2,
     ):
         self.position = np.array(
             position if position is not None else [0.0, 0.0, 0.0], dtype=float
@@ -16,6 +17,8 @@ class Object3D:
         )
         self.mass = mass
         self.forces = np.zeros(3, dtype=float)
+        self.friction = friction
+        self.on_ground = False
 
     def add_force(self, force):
         self.forces += np.array(force, dtype=float)
@@ -28,14 +31,38 @@ class Object3D:
 
     def update(self, dt, gravity=9.8, ground_height=0.25, bounce=0.5):
         self.add_force([0, -gravity * self.mass, 0])
+        self.apply_friction()
+
         acceleration = self.forces / self.mass
         initial_velocity = self.velocity
         self.velocity += acceleration * dt
         self.position += initial_velocity * dt + 0.5 * acceleration * dt * dt
         self.forces[:] = 0
 
-        if self.position[1] <= ground_height:
-            self.position[1] = ground_height
+        self.apply_bounce(bounce)
+        self.check_ground()
+
+    def check_ground(self):
+        if self.position[0] <= -19.5 or self.position[0] >= 19.5:
+            self.position[0] = min(max(self.position[0], -19.5), 19.5)
+            self.velocity[0] = 0
+
+        if self.position[2] <= -19.5 or self.position[2] >= 19.5:
+            self.position[2] = min(max(self.position[2], -19.5), 19.5)
+            self.velocity[2] = 0
+
+    def apply_friction(self):
+        if self.on_ground:
+            friction_force = -self.friction * self.velocity
+            friction_force[1] = 0
+            self.forces += friction_force
+
+    def apply_gravity(self, gravity):
+        self.add_force([0, -gravity * self.mass, 0])
+
+    def apply_bounce(self, bounce):
+        if self.position[1] <= bounce:
+            self.position[1] = bounce
             if self.velocity[1] < 0:
                 self.velocity[1] = -self.velocity[1] * bounce / (1 + self.mass)
                 if abs(self.velocity[1]) < 0.1:

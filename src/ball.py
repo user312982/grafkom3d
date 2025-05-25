37db3dcccc0f96649b4ae89c574bf5ce33347d61
thegouhund <alexandresapu1@gmail.com>
2025-05-26 02:38:29 +0800

    feat: add ball arrow

--- src/ball.py
+++ src/ball.py
@@ -1,14 +1,25 @@
 from rigid_body_3d import RigidBody3D
 import numpy as np
+from OpenGL.GL import glPushMatrix, glPopMatrix
+from OpenGL.GL import (
+    glBegin,
+    glEnd,
+    glVertex3f,
+    glColor3f,
+    GL_LINES,
+    glTranslatef,
+    glRotatef,
+    glLineWidth,
+)
 
 
 class Ball(RigidBody3D):
-    def __init__(self, position=None, velocity=None, mass=1.0, radius=0.4, **kwargs):
+    def __init__(self, position=None, velocity=None, mass=1.0, radius=0.4, force=20, **kwargs):
         super().__init__(position=position, velocity=velocity, mass=mass, **kwargs)
         self.radius = radius
         self.rotation_axis = [0.0, 0.0, 0.0]
         self.rotation_angle = 0.0
-        self.force = 20
+        self.force = force
         self.yaw = 0.0
 
     def update(self, dt, keymap, gravity, ground_height=0.4):
@@ -28,21 +39,23 @@ class Ball(RigidBody3D):
 
         if keymap.get("w", False):
             move_dir += forward
-            self.add_force(forward * self.force)
+            # self.add_force(forward * self.force)
         if keymap.get("s", False):
             move_dir -= forward
-            self.add_force(-forward * self.force)
+            # self.add_force(-forward * self.force)
         if keymap.get("d", False):
             move_dir -= right
-            self.add_force(-right * self.force)
+            # self.add_force(-right * self.force)
         if keymap.get("a", False):
             move_dir += right
-            self.add_force(right * self.force)
+            # self.add_force(right * self.force)
 
         if keymap.get("space", False):
             if self.on_ground:
-                self.velocity[1] = 5
-                self.on_ground = False
+                self.velocity = forward * self.force
+
+        if keymap.get("z", False):
+            self.velocity = np.array([0, 0, 0], dtype=np.float32)
 
         # Roll
         v = self.velocity.copy()
@@ -55,3 +68,28 @@ class Ball(RigidBody3D):
                 self.rotation_axis = axis / axis_norm
                 angle_delta = (speed * dt) / self.radius
                 self.rotation_angle += np.degrees(angle_delta)
+        
+
+    def draw_arrow(self):
+        glPushMatrix()
+        glTranslatef(
+            self.position[0],
+            self.position[1],
+            self.position[2],
+        )
+        glRotatef(self.yaw, 0, 1, 0)
+
+        glColor3f(0.0, 0.5, 2.0)
+        glLineWidth(3)
+        glBegin(GL_LINES)
+        glVertex3f(0, 0, 0)
+        glVertex3f(0, 0, 2)
+        glEnd()
+        glBegin(GL_LINES)
+        glVertex3f(0, 0, 2)
+        glVertex3f(0.3, 0, 1.0)
+        glVertex3f(0, 0, 2)
+        glVertex3f(-0.3, 0, 1.0)
+        glEnd()
+
+        glPopMatrix()
